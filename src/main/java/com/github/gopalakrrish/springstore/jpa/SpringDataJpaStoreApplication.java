8655936749ce3962eead595c0bc7c0ba1cb7c47f
Gopala Krishna <gopalakrrish15@gmail.com>
2026-02-07 17:03:30 +0530

    Initialize Spring Boot project with Spring Data JPA

---
+++ src/main/java/com/github/gopalakrrish/springstore/jpa/SpringDataJpaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.gopalakrrish.springstore.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaStoreApplication.class, args);
+	}
+
+}

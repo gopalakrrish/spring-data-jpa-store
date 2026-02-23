c13cb3c8f72f9b0449b482ba85e16ae875ac0766
Gopala Krishna <gopalakrrish15@gmail.com>
2026-02-24 04:35:48 +0530

    Add UserRepository and validate save/fetch operations in main method

--- src/main/java/com/github/gopalakrrish/springstore/jpa/SpringDataJpaStoreApplication.java
+++ src/main/java/com/github/gopalakrrish/springstore/jpa/SpringDataJpaStoreApplication.java
@@ -1,29 +1,28 @@
 package com.github.gopalakrrish.springstore.jpa;
 
-import com.github.gopalakrrish.springstore.jpa.entities.*;
+import com.github.gopalakrrish.springstore.jpa.entities.User;
+import com.github.gopalakrrish.springstore.jpa.repositories.UserRepository;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.math.BigDecimal;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDataJpaStoreApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(SpringDataJpaStoreApplication.class, args);
-
-		var product = new Product();
-		product.setId(1L);
-		product.setName("product1");
-		product.setDescription("this is product1");
-		product.setPrice(BigDecimal.TEN);
+		ApplicationContext context = SpringApplication.run(SpringDataJpaStoreApplication.class, args);
+		var userRepository = context.getBean(UserRepository.class);
 
 		var user = User.builder()
-						.name("user1")
-						.build();
+				.name("John")
+				.email("john@gmail.com")
+				.password("1234")
+				.build();
 
-		user.addToWishlist(product);
+		userRepository.save(user);
 
-		System.out.println(user);
+		userRepository.findAll().forEach(u -> System.out.println(u.getEmail()));
+
 	}
 
 }
